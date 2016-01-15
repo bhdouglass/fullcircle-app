fad251ba52c876de2ce583ac55755ca99a96c747
Brian Douglass <bhdouglass@gmail.com>
2016-01-15 01:34:53 -0500

    Updated issues list location

--- src/index.js
+++ src/index.js
@@ -2,16 +2,59 @@ var axios = require('axios');
 var moment = require('moment');
 
 function fetchIssues(callback) {
-  //axios.get('https://www.kimonolabs.com/api/dsun5vv4?&apikey=VcJUuOZizXZr5J7vwfFzff3Tt6m6q5t7&kimmodify=1')
-  axios.get('http://bhdouglass.com/fcm.json')
+  axios.get('http://dl.fullcirclemagazine.org/fcm-main.json')
   .then(function(res) {
     console.log('request was successful');
-    if (res.data.results) {
-      callback(null, res.data.results);
+
+    var issues = [];
+    for (var key in res.data) {
+      if (key != 'limit') {
+        var issue = res.data[key];
+        var id = key.replace('issue-', '');
+        while (id.length < 3) { //Add leading zeros
+          id = '0' + id;
+        }
+
+        issue.id = 'issue-' + id;
+
+        var downloads = [];
+        for (var lang in issue.links) {
+          downloads.push({
+            lang: lang,
+            link: issue.links[lang],
+          });
+        }
+
+        delete issue.links;
+        issue.downloads = downloads.sort(function(a, b) { //sort by language
+          var value = 0;
+          if (a.lang < b.lang) {
+            value = -1;
+          }
+          else if (a.lang > b.lang) {
+            value = 1;
+          }
+
+          return value;
+        });
+
+        issues.push(issue);
+      }
     }
-    else {
-      callback('Could not fetch issue list');
-    }
+
+    issues = issues.sort(function(a, b) { //reverse sort by id
+      var value = 0;
+      if (a.id > b.id) {
+        value = -1;
+      }
+      else if (a.id < b.id) {
+        value = 1;
+      }
+
+      return value;
+    });
+
+    callback(null, issues);
   }).catch(function(res) {
     console.error('request error: ' + JSON.stringify(res));
     callback('Could not fetch issue list');
